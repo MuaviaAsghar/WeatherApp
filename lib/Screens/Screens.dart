a26fd816b5902843922513b8e4739b37dcf5cb30
Muavia Asghar <newgamer445@gmail.com>
2024-04-29 10:23:37 +0500

    Weather Details

--- lib/Screens/Screens.dart
+++ lib/Screens/Screens.dart
@@ -7,13 +7,15 @@ import 'package:geocoding/geocoding.dart';
 import 'package:weather_app/api_key.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool isExpanded = false;
   final WeatherFactory _wf = WeatherFactory(openWeatherApiKey);
   Weather? _weather;
 
@@ -25,6 +27,25 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     _getLocationAndWeather();
+    _getStringData();
+  }
+
+  Future<String> _getStringData() async {
+    if (_weather != null) {
+      return 'Place Name: ${_weather!.areaName} [${_weather!.country}] (${_weather!.latitude}, ${_weather!.longitude}),\n'
+          'Date: ${_weather!.date},\n'
+          'Weather: ${_weather!.weatherMain}, ${_weather!.weatherDescription},\n'
+          'Temp: ${_weather!.temperature},\n'
+          'Temp (min): ${_weather!.tempMin},\n'
+          'Temp (max): ${_weather!.tempMax},\n'
+          'Temp (feels like): ${_weather!.tempFeelsLike},\n'
+          'Sunrise: ${_weather!.sunrise},\n'
+          'Sunset: ${_weather!.sunset},\n'
+          'Wind: speed ${_weather!.windSpeed}, degree: ${_weather!.windDegree}, gust ${_weather!.windGust},\n'
+          'Weather Condition code: ${_weather!.weatherConditionCode}';
+    } else {
+      return ''; 
+    }
   }
 
   Future<void> _getLocationAndWeather() async {
@@ -35,7 +56,6 @@ class _HomeScreenState extends State<HomeScreen> {
       return;
     }
 
-    // Request permission to access location
     LocationPermission permission = await Geolocator.requestPermission();
     if (permission == LocationPermission.denied) {
       _showLocationPermissionDeniedMessage();
@@ -168,7 +188,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                     height: 100,
                                   ),
                                   Text(
-                                    _weather!.weatherDescription!,
+                                    _weather!.weatherMain!,
                                     style: const TextStyle(
                                       fontSize: 28,
                                       fontWeight: FontWeight.bold,
@@ -190,6 +210,60 @@ class _HomeScreenState extends State<HomeScreen> {
                       ],
                     ),
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Weather Details ",
+                    style: TextStyle(
+                      color: Color(0xff181725),
+                      fontSize: 38,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isExpanded = !isExpanded;
+                      });
+                    },
+                    child: Icon(
+                      isExpanded
+                          ? Icons.keyboard_arrow_down
+                          : Icons.keyboard_arrow_right,
+                      color: Colors.black,
+                      size: 40,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            if (isExpanded)
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+                child: FutureBuilder<String>(
+                  future: _getStringData(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else {
+                      return Text(
+                        snapshot.data ??
+                            '', // Display the data or an empty string if null
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Color(0xff7C7C7C),
+                        ),
+                      );
+                    }
+                  },
+                ),
+              ),
           ],
         ),
       ),
